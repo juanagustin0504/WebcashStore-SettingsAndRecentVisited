f0a14891d2dc80bd2d020994faf450a812e0ec68
juanagustin0504 <juanagustin@naver.com>
2020-01-20 18:16:25 +0900

    Add roundedRect

--- WebcashStore_SettingsAndRecentVisited/View/Main/Settings/SettingsViewController.swift
+++ WebcashStore_SettingsAndRecentVisited/View/Main/Settings/SettingsViewController.swift
@@ -12,23 +12,55 @@ class SettingsViewController: UIViewController {
     
     
     @IBOutlet weak var navView: UIView!
-    @IBOutlet weak var contentsView: UIView!
+    @IBOutlet weak var settingsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        let maskPath = UIBezierPath(roundedRect: settingsView.bounds, byRoundingCorners: [UIRectCorner.topLeft, UIRectCorner.topRight], cornerRadii: CGSize(width: 30.0, height: 30.0))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = settingsView.bounds
+        maskLayer.path = maskPath.cgPath
+        
+//        maskLayer.applySketchShadow(color: .black, alpha: 0.4, x: 0, y: -5, blur: 5, spread: 0)
+//        settingsView.layer.applySketchShadow(color: .black, alpha: 0.4, x: 0, y: -10, blur: 5, spread: 0)
+        
+        settingsView.layer.mask = maskLayer
     }
     
-
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
     }
-    */
-
 }
